92a600b1ba575d46035ffdf745b44b595e5218c1
Hung Nguyen <hungnh.uet@gmail.com>
2016-08-23 18:14:13 +0700

    big refactor

---
+++ common/common-model/src/main/java/uet/hungnh/template/model/BaseEntity.java
@@ -0,0 +1,54 @@
+package uet.hungnh.template.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date createdDate;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date updatedDate;
+
+    @PrePersist
+    @PreUpdate
+    public void onUpdate() {
+        if (createdDate == null) {
+            createdDate = new Date();
+        }
+        updatedDate = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
---
+++ sample/sample-model/src/main/java/uet/hungnh/template/model/Sample.java
@@ -0,0 +1,24 @@
+package uet.hungnh.template.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"name"}, name = "sample_name_unq")})
+public class Sample extends BaseEntity {
+
+    private static final long serialVersionUID = 1427168249598037052L;
+
+    @Column(nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
