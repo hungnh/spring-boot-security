ebf60faf0ede9117b7f2f37479b9fa073c75f4c2
Hung Nguyen <hungnh.uet@gmail.com>
2016-09-20 18:12:04 +0700

    spring boot actuator graphite

---
+++ sample/sample-service/src/main/java/uet/hungnh/config/MetricsConfig.java
@@ -0,0 +1,53 @@
+package uet.hungnh.config;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public Graphite graphite(
+            @Value("${graphite.host}") String graphiteHost,
+            @Value("${graphite.port}") int graphitePort
+    ) {
+        return new Graphite(graphiteHost, graphitePort);
+    }
+
+    @Bean
+    public GraphiteReporter graphiteReporter(Graphite graphite, MetricRegistry registry) {
+        GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+                .prefixedWith("uet.hungnh")
+                .convertRatesTo(TimeUnit.MILLISECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .filter(MetricFilter.ALL)
+                .build(graphite);
+
+        reporter.start(5, TimeUnit.SECONDS);
+
+        return reporter;
+    }
+
+    @Bean
+    public MemoryUsageGaugeSet memoryUsageGaugeSet(MetricRegistry registry) {
+        MemoryUsageGaugeSet memoryUsageGaugeSet = new MemoryUsageGaugeSet();
+        registry.register("sample-app.memory", memoryUsageGaugeSet);
+        return memoryUsageGaugeSet;
+    }
+
+    @Bean
+    public ThreadStatesGaugeSet threadStatesGaugeSet(MetricRegistry registry) {
+        ThreadStatesGaugeSet threadStatesGaugeSet = new ThreadStatesGaugeSet();
+        registry.register("sample-app.thread-state", threadStatesGaugeSet);
+        return threadStatesGaugeSet;
+    }
+}
