472303b5871f4be32f497974df3bb69731b2e9f1
hungnh <hungnh.uet@gmail.com>
2016-10-18 07:42:33 +0700

    hazelcast token caching

---
+++ hazelcast/hazelcast-client/hazelcast-client-autoconfig/src/main/java/uet/hungnh/config/HazelcastClientConfiguration.java
@@ -0,0 +1,27 @@
+package uet.hungnh.config;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HazelcastClientConfiguration {
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setInstanceName("uet.hungnh.caching.hz-client");
+
+        GroupConfig groupConfig = clientConfig.getGroupConfig();
+        groupConfig.setName("hz-caching-group");
+        groupConfig.setPassword("123qweasd");
+
+        ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+        networkConfig.addAddress("192.168.1.68");
+
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+}
--- hazelcast/hazelcast-member/hazelcast-member-autoconfig/src/main/java/uet/hungnh/config/HazelcastMemberConfiguration.java
+++ hazelcast/hazelcast-member/hazelcast-member-autoconfig/src/main/java/uet/hungnh/config/HazelcastMemberConfiguration.java
@@ -1,28 +1,35 @@
 package uet.hungnh.config;
 
 import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.hazelcast.HazelcastInstanceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PreDestroy;
+
 @Configuration
-public class HazelcastConfiguration {
+public class HazelcastMemberConfiguration {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(HazelcastMemberConfiguration.class);
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
-        config.setInstanceName("uet.hungnh.caching");
+        config.setInstanceName("uet.hungnh.caching.hz-member");
 
         GroupConfig groupConfig = config.getGroupConfig();
-        groupConfig.setName("token-cache-group");
+        groupConfig.setName("hz-caching-group");
         groupConfig.setPassword("123qweasd");
 
         NetworkConfig networkConfig = config.getNetworkConfig();
         JoinConfig joinConfig = networkConfig.getJoin();
         joinConfig.getMulticastConfig().setEnabled(false);
         joinConfig.getTcpIpConfig().setEnabled(true);
-        joinConfig.getTcpIpConfig().addMember("192.168.1.18");
+        joinConfig.getTcpIpConfig().addMember("192.168.1.68");
 
         config.addMapConfig(tokenMapConfig());
 
@@ -35,5 +42,11 @@ public class HazelcastConfiguration {
         tokenMapConfig.setEvictionPolicy(EvictionPolicy.LRU);
         tokenMapConfig.setTimeToLiveSeconds(60 * 60);
         return tokenMapConfig;
+    }
+
+    @PreDestroy
+    public void stopAllInstances() {
+        LOGGER.info("Shutting down all Hazelcast instances!");
+        Hazelcast.shutdownAll();
     }
 }
